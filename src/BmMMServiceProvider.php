7450c3bf5fdd6e34a7e8e9e337df779196fe1ef6
Diego Gonella <diego@meemba.com.ar>
2019-05-05 18:17:45 -0300

    First commit

---
+++ src/BmMMServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace diegonella\bmmm;
+
+use Illuminate\Support\ServiceProvider;
+
+class BmMMServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
